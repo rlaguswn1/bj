35b77cf322abd849f3bd08c1779b1ea5ab7d8eeb
rlaguswn1 <94452359+rlaguswn1@users.noreply.github.com>
2024-07-04 16:20:53 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        char c = s.next().charAt(0);
+        
+        System.out.print((int) c);
+      
+       
+   }
+}
