ff21a50f0bdc94cdff2302f5c2777cf86d9e373d
rlaguswn1 <94452359+rlaguswn1@users.noreply.github.com>
2024-07-04 17:11:27 +0900

    Create 학점계산-통.java

---
+++ 백준/Bronze/2754. 학점계산/학점계산-통.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String score = s.next();
+
+        switch(score){
+            case "A+":
+                    System.out.print(4.3);
+                    break;
+            case "A0":
+                    System.out.print(4.0);
+                    break;
+            case "A-":
+                    System.out.print(3.7);
+                    break;                
+            case "B+":
+                    System.out.print(3.3);
+                    break;      
+            case "B0":
+                    System.out.print(3.0);
+                    break;                
+            case "B-":
+                    System.out.print(2.7);
+                    break;
+            case "C+":
+                    System.out.print(2.3);
+                    break;   
+            case "C0":
+                    System.out.print(2.0);
+                    break;         
+            case "C-":
+                    System.out.print(1.7);
+                    break;  
+            case "D+":
+                    System.out.print(1.3);
+                    break;       
+            case "D0":
+                    System.out.print(1.0);
+                    break;           
+            case "D-":
+                    System.out.print(0.7);
+                    break;          
+            case "F":
+                    System.out.print(0.0);
+                    break;                  
+        }
+    }
+}
