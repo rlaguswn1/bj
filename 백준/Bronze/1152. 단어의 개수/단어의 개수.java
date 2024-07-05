be2a553f3c5ab905c5b041eb76c0d9e302482839
rlaguswn1 <94452359+rlaguswn1@users.noreply.github.com>
2024-07-05 16:48:55 +0900

    [Bronze II] Title: 단어의 개수, Time: 488 ms, Memory: 30832 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -7,15 +7,7 @@ class Main {
         Scanner s = new Scanner(System.in);
         String line = s.nextLine();
 
-        // 1. 문장 앞뒤 공백 자르기, 공백 단위 스플릿, 공백만 1개인경우 0 출력, 아닌 경우 배열 길이 출력
-        String[] word = line.trim().split(" ");   
-        
-        if (word[0].isEmpty()==true){
-            System.out.print(0);
-        }
-            
-        else {
-            System.out.println(word.length);
-        }             
+        StringTokenizer t = new StringTokenizer(line);		
+		System.out.print(t.countTokens());            
     }        
 }
