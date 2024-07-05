16b75914e4636b191e3727d20834eba115b5c9eb
rlaguswn1 <94452359+rlaguswn1@users.noreply.github.com>
2024-07-05 13:10:32 +0900

    [Bronze V] Title: 이상한 기호, Time: 184 ms, Memory: 17644 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        System.out.print((a+b)*(a-b));
+    }
+}
