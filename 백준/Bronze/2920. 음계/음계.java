08d800ddf61ca82fc8eb97a6dd6660576cdb4bf1
rlaguswn1 <94452359+rlaguswn1@users.noreply.github.com>
2024-07-15 13:45:56 +0900

    Update 음계.java

--- 백준/Bronze/2920. 음계/음계.java
+++ 백준/Bronze/2920. 음계/음계.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-//나중에 이어서
 class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
@@ -22,19 +21,22 @@ class Main {
 다음 자리가 +1이 아니면 바로 mixed 출력
 다음 자리가 +1이면 넘어가기
 
-다 체크하고나면 ascending 출력 >> 이걸 어떻게?
-
 마지막까지 쭉 체크
 
 2. 처음이 8
 다음이 7이 아니면 mixed
+다음 자리가 바로 아래 숫자면 넘어가기
 
-3. 처음이 2~7
+마지막까지 쭉 체크
+
+3. 처음이 2~7 - else
 - mixed
+
+1,2는 if에서 이미 arr[0]을 확인하고 넘어온 셈이니 arr[1]부터 체크
 */
 
         if (a==1){
-            for (int i=0; i < arr.length; i++) {
+            for (int i=1; i < arr.length; i++) {
                 if (arr[i]==i+1){
                     if (i==7) {
                     System.out.println("ascending");
@@ -44,14 +46,12 @@ class Main {
                 } else if (arr[i]!=i+1){
                     System.out.println("mixed");
                     break;
-                }
-
-                
+                }               
                 
             }
             
         } else if (a==8) {
-            for (int j=0; j<arr.length; j++) {
+            for (int j=1; j<arr.length; j++) {
                 if (arr[j]==8-j){
                     if (j==7) {
                     System.out.println("descending");
@@ -66,7 +66,5 @@ class Main {
         } else {
             System.out.println("mixed");
         }
-
-
     }        
 }
